b08797f7c1c3a51f1c455a9e81e0cffab2804db8
orcunTasdemir <orcntasdemir@gmail.com>
2022-06-19 14:30:38 -0500

    tailwind css changes

--- web/src/layouts/BlogLayout/BlogLayout.tsx
+++ web/src/layouts/BlogLayout/BlogLayout.tsx
@@ -9,7 +9,58 @@ const BlogLayout = ({ children }: BlogLayoutProps) => {
 
   return (
     <>
-      <header>
+      <header className="relative flex justify-between items-center py-4 px-8 bg-blue-700 text-white">
+        <h1 className="text-5xl font-semibold tracking-tight">
+          <Link
+            className="text-blue-400 hover:text-blue-100 transition duration-100"
+            to={routes.home()}
+          >
+            Redwood Blog
+          </Link>
+        </h1>
+        <nav>
+          <ul className="relative flex items-center font-light">
+            <li>
+              <Link
+                className="py-2 px-4 hover:bg-blue-600 transition duration-100 rounded"
+                to={routes.about()}
+              >
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="py-2 px-4 hover:bg-blue-600 transition duration-100 rounded"
+                to={routes.contact()}
+              >
+                Contact
+              </Link>
+            </li>
+            <li>
+              {isAuthenticated ? (
+                <div>
+                  <button type="button" onClick={logOut} className="py-2 px-4">
+                    Logout
+                  </button>
+                </div>
+              ) : (
+                <Link to={routes.login()} className="py-2 px-4">
+                  Login
+                </Link>
+              )}
+            </li>
+          </ul>
+          {isAuthenticated && (
+            <div className="absolute bottom-1 right-0 mr-12 text-xs text-blue-300">
+              {currentUser.email}
+            </div>
+          )}
+        </nav>
+      </header>
+      <main className="max-w-4xl mx-auto p-12 bg-white shadow rounded-b">
+        {children}
+      </main>
+      {/* <header>
         <div className="flex-between">
           <h1>
             <Link to={routes.home()}>Redwood Blog</Link>
@@ -40,7 +91,7 @@ const BlogLayout = ({ children }: BlogLayoutProps) => {
           </ul>
         </nav>
       </header>
-      <main>{children}</main>
+      <main>{children}</main> */}
     </>
   )
 }
--- web/src/pages/ContactPage/ContactPage.tsx
+++ web/src/pages/ContactPage/ContactPage.tsx
@@ -52,6 +52,79 @@ const ContactPage = () => {
       <MetaTags title="Contact" description="Contact page" />
 
       <Toaster />
+      <Form
+        onSubmit={onSubmit}
+        config={{ mode: 'onBlur' }}
+        error={error}
+        formMethods={formMethods}
+      >
+        <FormError
+          error={error}
+          wrapperClassName="py-4 px-6 rounded-lg bg-red-100 text-red-700"
+          listClassName="list-disc ml-4"
+          listItemClassName=""
+        />
+        <Label
+          name="name"
+          className="block text-gray-700 uppercase text-sm"
+          errorClassName="block uppercase text-sm text-red-700"
+        >
+          Name
+        </Label>
+        <TextField
+          name="name"
+          validation={{ required: true }}
+          className="border rounded-sm px-2 py-1 outline-none"
+          errorClassName="border rounded-sm px-2 py-1 border-red-700 outline-none"
+        />
+        <FieldError name="name" className="block text-red-700" />
+
+        <Label
+          name="email"
+          className="block mt-8 text-gray-700 uppercase text-sm"
+          errorClassName="block mt-8 text-red-700 uppercase text-sm"
+        >
+          Email
+        </Label>
+        <TextField
+          name="email"
+          validation={{
+            required: true,
+            pattern: {
+              value: /[^@]+@[^.]+\..+/,
+              message: 'Please enter a valid email address',
+            },
+          }}
+          className="border rounded-sm px-2 py-1"
+          errorClassName="border rounded-sm px-2 py-1 border-red-700 outline-none"
+        />
+        <FieldError name="email" className="block text-red-700" />
+
+        <Label
+          name="message"
+          className="block mt-8 text-gray-700 uppercase text-sm"
+          errorClassName="block mt-8 text-red-700 uppercase text-sm"
+        >
+          Message
+        </Label>
+        <TextAreaField
+          name="message"
+          validation={{ required: true }}
+          className="block border rounded-sm px-2 py-1"
+          errorClassName="block border rounded-sm px-2 py-1 border-red-700 outline-none"
+        />
+        <FieldError name="message" className="block text-red-700" />
+
+        <Submit
+          className="block bg-blue-700 text-white mt-8 px-4 py-2 rounded"
+          disabled={loading}
+        >
+          Save
+        </Submit>
+      </Form>
+      {/* <MetaTags title="Contact" description="Contact page" />
+
+      <Toaster />
 
       <Form
         onSubmit={onSubmit}
@@ -98,7 +171,7 @@ const ContactPage = () => {
         <FieldError name="message" className="error"></FieldError>
 
         <Submit disabled={loading}>Submit</Submit>
-      </Form>
+      </Form> */}
     </>
   )
 }
